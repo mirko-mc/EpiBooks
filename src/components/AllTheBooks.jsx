7942cc32e344c763bf3b29f6f8901c4c6f29f8b7
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-06-29 17:25:49 +0000

    - added useState - added handleClick RES = too many re-renders

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -6,35 +6,40 @@ import Scifi from "../data/scifi.json";
 import { BookCard } from "./BookCard";
 
 export const AllTheBooks = (genre) => {
-  // console.log(Fantasy);
   console.log("ALLTHEBOOKS => genre\n", genre);
   switch (genre) {
     case "fantasy":
       genre = Fantasy.slice(0, 24);
-      console.log("ALLTHEBOOKS SWITCH FANTASY => genre\n", genre);
+      console.log("ALLTHEBOOKS SWITCH FANTASY => genre");
+      // console.table(genre);
       break;
     case "history":
       genre = History.slice(0, 24);
-      console.log("ALLTHEBOOKS SWITCH HISTORY=> genre\n", genre);
+      console.log("ALLTHEBOOKS SWITCH HISTORY=> genre");
+      // console.table(genre);
       break;
     case "horror":
       genre = Horror.slice(0, 24);
-      console.log("ALLTHEBOOKS SWITCH HORROR=> genre\n", genre);
+      console.table("ALLTHEBOOKS SWITCH HORROR=> genre");
+      // console.table(genre);
       break;
     case "romance":
       genre = Romance.slice(0, 24);
-      console.log("ALLTHEBOOKS SWITCH ROMANCE=> genre\n", genre);
+      console.log("ALLTHEBOOKS SWITCH ROMANCE=> genre");
+      // console.table(genre);
       break;
     case "scifi":
       genre = Scifi.slice(0, 24);
-      console.log("ALLTHEBOOKS SWITCH SCIFI=> genre\n", genre);
+      console.table("ALLTHEBOOKS SWITCH SCIFI=> genre");
+      // console.table(genre);
       break;
 
     default:
       genre = Fantasy.slice(0, 24);
-      console.log("ALLTHEBOOKS SWITCH DEFAULT=> genre\n", genre);
+      console.log("ALLTHEBOOKS SWITCH DEFAULT => genre");
+      // console.table(genre);
       break;
   }
-  // console.log(Fantasy.length);
+  // console.log(genre.length);
   return genre.map((book) => <BookCard key={book.asin} props={book} />);
 };
--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,6 +1,17 @@
 import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import { AllTheBooks } from "./AllTheBooks";
+import { useState } from "react";
+
 export const MyNav = () => {
+  let [genre, setGenre] = useState("fantasy");
+  function handleClick(gender){
+    console.log("HANDLECLICK => genre\n",genre);
+    console.log("HANDLECLICK => gender\n",gender);
+    setGenre(gender);
+    console.log("HANDLECLICK => genre\n",genre);
+    console.log("HANDLECLICK => gender\n",gender);
+    AllTheBooks(genre)
+  }
   return (
     <header>
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -12,11 +23,11 @@ export const MyNav = () => {
               <Nav.Link href="#">About</Nav.Link>
               <Nav.Link href="#">Browse</Nav.Link>
               <NavDropdown title="Genre" id="genre">
-                <NavDropdown.Item href="#" onClick={() => AllTheBooks("fantasy")}>Fantasy</NavDropdown.Item>
-                <NavDropdown.Item href="#" onClick={() => AllTheBooks('history')}>History</NavDropdown.Item>
-                <NavDropdown.Item href="#" onClick={() => AllTheBooks("horror")}>Horror</NavDropdown.Item>
+                {/* <NavDropdown.Item href="#" onClick={() => AllTheBooks("fantasy")}>Fantasy</NavDropdown.Item> */}
+                <NavDropdown.Item href="#" onClick={handleClick("history")}>History</NavDropdown.Item>
+                {/* <NavDropdown.Item href="#" onClick={() => AllTheBooks("horror")}>Horror</NavDropdown.Item>
                 <NavDropdown.Item href="#" onClick={() => AllTheBooks("romance")}>Romance</NavDropdown.Item>
-                <NavDropdown.Item href="#" onClick={() => AllTheBooks("scifi")}>Scifi</NavDropdown.Item>
+                <NavDropdown.Item href="#" onClick={() => AllTheBooks("scifi")}>Scifi</NavDropdown.Item> */}
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
